58ff963a165a6cb7746930012c6b205b82f932f2
S Anand <root.node@gmail.com>
2025-04-17 17:08:42 +0800

    FIX: Switch to aipipe and GPT 4.1 Nano

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+import { getProfile } from "https://aipipe.org/aipipe.js";
 import { llmviz } from "./llmviz.js";
+
+const { token, email } = getProfile();
+if (!token) window.location = `https://aipipe.org/login?redirect=${window.location.href}`;
 
 const $output = document.querySelector("#output");
 const num1 = new Intl.NumberFormat("en-US", {
@@ -14,26 +18,21 @@ document.querySelector("form").addEventListener("submit", async (e) => {
   $output.innerHTML = `<div class="spinner-border" role="status"></div>`;
 
   let result;
-  let key = document.querySelector("#key").value;
-  // OpenAI keys start with sk_. LLMProxy keys don't, and require a :app-name at the end
-  if (!key.startsWith("sk_")) key += ":llmviz";
   try {
-    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+    const response = await fetch("https://aipipe.org/openrouter/v1/chat/completions", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Cache-Control": "no-cache",
-        Authorization: `Bearer ${key}`,
+        Authorization: `Bearer ${token}`,
       },
-      credentials: "include",
       body: JSON.stringify({
-        model: "gpt-4o-mini",
+        model: "openai/gpt-4.1-nano",
         messages: [
-          { role: "system", content: document.querySelector("#system").value },
           { role: "user", content: document.querySelector("#user").value },
         ],
         logprobs: true,
         top_logprobs: 5,
+        max_tokens: 200,
         temperature: +document.querySelector("#temperature").value,
         top_p: +document.querySelector("#top_p").value,
       }),
